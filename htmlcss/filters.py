c80d05c1b0ea6ca48a8c650062f2f8c557f666e3
Geovanni Tinoco <116227965+GeoT82@users.noreply.github.com>
2023-09-19 19:53:52 -0400

    Delete htmlcss directory

--- htmlcss/filters.py
+++
@@ -1,7 +0,0 @@
-import django_filters
-from .models import Restaurant
-
-class RestaurantFilter(django_filters.FilterSet):
-    class Meta:
-        model = Restaurant
-        fields = ['businessName']
