e427b9139b489967b4cbe7d892fa28bea41e920c
Abdullahi Ayantayo <ayantayoabdullah@gmail.com>
2023-11-21 16:37:42 -0500

    Implement Lesson.java to connect to the database

---
+++ LessonDAO.java
@@ -0,0 +1,101 @@
+import java.io.FileNotFoundException;
+
+import java.lang.System;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+//import java.sql.Connection;
+//import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+//import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Servlet implementation class Connect
+ */
+@WebServlet("/LessonDAO")
+public class LessonDAO 
+{
+	private static final long serialVersionUID = 1L;
+	private Connection connect = null;
+	private Statement statement = null;
+	private PreparedStatement preparedStatement = null;
+	private ResultSet resultSet = null;
+	
+	public LessonDAO(){}
+	
+	/** 
+	 * @see HttpServlet#HttpServlet()
+     */
+    protected void connect_func() throws SQLException {
+    	//uses default connection to the database
+        if (connect == null || connect.isClosed()) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                throw new SQLException(e);
+            }
+            connect = (Connection) DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/webDB?allowPublicKeyRetrieval=true&useSSL=false&user=root&password=pass1234");
+            System.out.println(connect);
+        }
+    }
+    
+    public boolean database_login(String email, String password) throws SQLException{
+    	try {
+    		connect_func("root","pass1234");
+    		String sql = "select * from user where email = ?";
+    		preparedStatement = connect.prepareStatement(sql);
+    		preparedStatement.setString(1, email);
+    		ResultSet rs = preparedStatement.executeQuery();
+    		return rs.next();
+    	}
+    	catch(SQLException e) {
+    		System.out.println("failed login");
+    		return false;
+    	}
+    }
+	//connect to the database 
+    public void connect_func(String username, String password) throws SQLException {
+        if (connect == null || connect.isClosed()) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                throw new SQLException(e);
+            }
+            connect = (Connection) DriverManager
+  			      .getConnection("jdbc:mysql://127.0.0.1:3306/webDB?allowPublicKeyRetrieval=true&"
+  			          + "useSSL=false&user=" + username + "&password=" + password);
+            System.out.println(connect);
+        }
+    }
+    
+    
+    protected void disconnect() throws SQLException {
+        if (connect != null && !connect.isClosed()) {
+        	connect.close();
+        }
+    }
+    
+    public void addLesson(Lesson lesson) {
+        Lesson.setLessonID(lesson.getLessonID(), Lesson);
+    }
+
+    public Lesson getLessonID(int lessonID) {
+        return Lesson.getLessonID(lessonID);
+    }
+
+    public void updateLesson(Lesson lesson) {
+        Lesson.setLessonID(Lesson.getLessonID(), lesson);
+    }
