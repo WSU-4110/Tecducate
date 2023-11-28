791299091a1f2ab251e7d8240c75ccb26f7ef0be
Abdullahi Ayantayo <ayantayoabdullah@gmail.com>
2023-11-28 09:48:20 -0500

    Rearranging my directory

--- Tecducate/src/main/java/Lesson.java
+++ Tecducate/src/main/java/Lesson.java
@@ -1,3 +1,48 @@
-public interface Lesson {
-    void displayContent();
-}
+public class Lesson 
+{
+
+	    protected int lessonID;
+	    protected String details;
+	    protected String sample;
+	 
+	    //constructors
+	    public Lesson() {
+	    }
+	
+	    
+	    public Lesson(int lessonID, String details, String sample) 
+	    {
+	    	setLessonID(lessonID);
+	    	setDetails(details);
+	    	setSample(sample);
+	    }
+	 
+	    
+	    
+	   //Main variable set and get methods
+	    public int getLessonID() {
+	        return lessonID;
+	    }
+	    public void setLessonID(String lessonID) {
+	        this.lessonID = lessonID;
+	    }
+	    
+	    
+	    public String getDetails() {
+	        return details;
+	    }
+	    public void setDetails(String details) {
+	        this.details = details;
+	    }
+
+	    
+	    public String getSample() {
+	        return sample;
+	    }
+	    public void setSample(String sample) {
+	        this.sample = sample;
+	    }
+	    	    
+	  
+	   
+	}
