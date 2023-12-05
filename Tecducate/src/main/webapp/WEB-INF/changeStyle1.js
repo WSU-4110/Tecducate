dbaabf7e81ab041deb7f9bef073ffe4f7add1a3d
Maria Alsamaien <hd8984@wayne.edu>
2023-12-04 20:27:35 -0500

    Created a copy of the changeStyle1

---
+++ Tecducate/src/main/webapp/WEB-INF/changeStyle1.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+ /* This function will run every time the 'style' button is clicked. */
+
+function toggleStyles() {
+  const body = document.querySelector("body")
+  body.classList.toggle("style")
+
+  const changeStyleButton = document.querySelector("#change-style-button")
+
+  if (body.classList.contains("style")) {
+    changeStyleButton.innerText = "Style"
+  } else {
+    changeStyleButton.innerText = "Style"
+  }
+  
+  
+}
+
